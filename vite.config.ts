1c8bf88fc5c88ddd3452f12ec3f3b918f5d66ec8
NH-99-FE <lianglh_only@163.com>
2025-04-21 20:06:10 +0800

    element-plus按需导入

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig, loadEnv } from 'vite';
 import type { UserConfig, ConfigEnv } from 'vite';
 import { fileURLToPath } from 'url';
+import AutoImport from "unplugin-auto-import/vite";
+import Icons from "unplugin-icons/vite";
+import Components from "unplugin-vue-components/vite";
+import IconsResolver from "unplugin-icons/resolver";
+import ElementPlus from "unplugin-element-plus/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 
@@ -21,6 +27,26 @@ export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
       vue(),
       // jsx文件编译插件
       vueJsx(),
+      // 开启ElementPlus自动引入CSS
+      ElementPlus({}),
+      // 自动引入组件及ICON
+      AutoImport({
+          resolvers: [IconsResolver(), ElementPlusResolver()],
+          dts: fileURLToPath(
+              new URL("./types/auto-imports.d.ts", import.meta.url),
+          ),
+      }),
+      // 自动注册组件
+      Components({
+          resolvers: [IconsResolver(), ElementPlusResolver()],
+          dts: fileURLToPath(
+              new URL("./types/components.d.ts", import.meta.url),
+          ),
+      }),
+      // 自动安装图标
+      Icons({
+          autoInstall: true,
+      }),
     ],
     // 运行后本地预览的服务器
     server: {
